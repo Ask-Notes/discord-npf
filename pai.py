fc8e3ec4406c36822d2c469eac44e52a1334fc61
kakasibanzai <kyuuriurutora01@gmail.com>
2023-02-15 11:56:25 +0900

    oppai

---
+++ pai.py
@@ -0,0 +1,11 @@
+import discord
+class kintama(): 
+    async def on_message(message):
+        channel = message.channel
+        content = message.content
+    
+        if channel.id == 983318451084984430:
+            if content == "金谷":
+                await channel.send("消えろｄさっだｄさだｓｄ")
+            
+
