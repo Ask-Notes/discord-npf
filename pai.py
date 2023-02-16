9ac616917c0709038ef9d7f20b8654acc5ea0f56
kakasibanzai <kyuuriurutora01@gmail.com>
2023-02-16 11:47:23 +0900

    ぱいぱいできたかもしれない

--- pai.py
+++ pai.py
@@ -7,5 +7,18 @@ class Kintama():
     
         if channel.id == 986992160719130654:
                 return "消えろｄさっだｄさだｓｄ"
+            
+
+class ayaka():
+    def messager(atk,ct,ctd,elementDmg):
+        channel = atk.channel   
+        content = atk.content
+        
+        tenpu = 1.09
+        if channel.id == 1075291090724323348 :
+            dmg = (atk * tenpu)*(1+elementDmg)*(1+(ct*ctd))
+            return dmg
+            
+            
          
 
