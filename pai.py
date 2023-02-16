bb4b4c534d41650908cdc43f34151b0232dc0467
kakasibanzai <kyuuriurutora01@gmail.com>
2023-02-16 14:24:38 +0900

    ごみごみ

--- pai.py
+++ pai.py
@@ -1,24 +1,20 @@
 import discord
 
-class Kintama(): 
+
+class Kintama():
     def messager(message):
         channel = message.channel
         content = message.content
-    
+
         if channel.id == 986992160719130654:
-                return "消えろｄさっだｄさだｓｄ"
-            
+            return "消えろｄさっだｄさだｓｄ"
+
 
 class ayaka():
-    def messager(atk,ct,ctd,elementDmg):
-        channel = atk.channel   
+    def messager(atk, ct, ctd, elementDmg):
+        channel = atk.channel
         content = atk.content
-        
         tenpu = 1.09
-        if channel.id == 1075291090724323348 :
+        if channel.id == 1075291090724323348:
             dmg = (atk * tenpu)*(1+elementDmg)*(1+(ct*ctd))
             return dmg
-            
-            
-         
-
